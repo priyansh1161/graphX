d3d4d93308b78dd9f25bfb87405fc05819a3e156
priyansh gupta <priyanshgupta1161@gmail.com>
2017-10-07 13:41:12 +0530

    Major dirr changes

--- graph.go
+++ graph.go
@@ -45,15 +45,3 @@ func(g *Graph) String() {
 //	g.adj[fromIndex].Remove(toIndex)
 //}
 
-func main() {
-	g := new(Graph)
-	g.addNode(&Node{value:1})
-	g.addNode(&Node{value:2})
-	g.addNode(&Node{value:3})
-	g.addNode(&Node{value:4})
-	g.addEdge(0, 1)
-	g.addEdge(2, 3)
-	g.addEdge(1, 3)
-	g.addEdge(2, 0)
-	fmt.Print(g)
-}
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	g := new(Graph)
+	g.addNode(&Node{value:1})
+	g.addNode(&Node{value:2})
+	g.addNode(&Node{value:3})
+	g.addNode(&Node{value:4})
+	g.addEdge(0, 1)
+	g.addEdge(2, 3)
+	g.addEdge(1, 3)
+	g.addEdge(2, 0)
+	fmt.Print(g)
 }
